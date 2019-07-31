273f5e5260bf58628a4de6acbc3eba936be58e93
kevinikos <kevin.nikolas123@gmail.com>
2019-07-31 17:52:17 +0200

    Add sidebar

--- js/wrapper-menu.js
+++ js/wrapper-menu.js
@@ -1,8 +1,10 @@
 function main() {
   const wrapperMenu = document.querySelector(".wrapper-menu");
+  const sidebar = document.querySelector(".sidebar");
 
   wrapperMenu.addEventListener("click", function() {
     wrapperMenu.classList.toggle("open");
+    sidebar.classList.toggle("show");
   });
 }
 
